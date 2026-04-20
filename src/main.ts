235a5aa2ba8288d4ab7a1510b0a8fcb9ec5d8c43
RadonCoding <henry.welch@reborn.com>
2026-04-20 18:23:03 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,48 @@
 import "./style.css";
 
-for (const element of document.getElementsByClassName("copy")) {
-  if (!(element instanceof HTMLElement)) continue;
-
-  element.addEventListener("click", () => {
-    const content = element.getAttribute("data-copy");
-
-    if (!content) return;
-
-    navigator.clipboard.writeText(content);
-  });
-}
-
 const tooltip = document.createElement("div");
 tooltip.id = "tooltip";
 document.body.appendChild(tooltip);
 
-let timeout: ReturnType<typeof setTimeout>;
+let tooltipTimeout: ReturnType<typeof setTimeout>;
+
+const update = (el: HTMLElement) => {
+  const rect = el.getBoundingClientRect();
+  tooltip.style.left = `${rect.left + window.scrollX + rect.width / 2}px`;
+  tooltip.style.top = `${rect.top + window.scrollY}px`;
+};
 
 for (const element of document.querySelectorAll<HTMLElement>(
   "[data-tooltip]",
 )) {
-  element.addEventListener("pointerenter", () => {
-    clearTimeout(timeout);
-    const rect = element.getBoundingClientRect();
-    tooltip.textContent = element.getAttribute("data-tooltip");
-    tooltip.classList.add("tooltip--visible");
-    tooltip.style.left = `${rect.left + rect.width / 2}px`;
-    tooltip.style.top = `${rect.top}px`;
-  });
+  element.addEventListener("click", (event) => {
+    clearTimeout(tooltipTimeout);
 
-  element.addEventListener("pointerleave", () => {
-    timeout = setTimeout(
-      () => tooltip.classList.remove("tooltip--visible"),
-      3000,
-    );
+    const content = element.getAttribute("data-tooltip");
+    const copy = element.getAttribute("data-copy");
+    const href = element.getAttribute("href");
+
+    tooltip.textContent = content;
+    tooltip.classList.add("tooltip--visible");
+    update(element);
+
+    element.setAttribute("data-state", "success");
+
+    setTimeout(() => {
+      element.removeAttribute("data-state");
+    }, 1000);
+
+    tooltipTimeout = setTimeout(() => {
+      tooltip.classList.remove("tooltip--visible");
+    }, 1000);
+
+    if (copy) {
+      navigator.clipboard.writeText(copy);
+    } else if (href) {
+      window.open(href, "_blank");
+    }
+
+    event.preventDefault();
   });
 }
 
