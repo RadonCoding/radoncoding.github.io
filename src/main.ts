a6c97adb48492c06df55967415f2717b2b5d30a0
RadonCoding <henry.welch@reborn.com>
2026-04-20 16:17:39 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,35 @@ for (const element of document.getElementsByClassName("copy")) {
     navigator.clipboard.writeText(content);
   });
 }
+
+const tooltip = document.createElement("div");
+tooltip.id = "tooltip";
+document.body.appendChild(tooltip);
+
+let timeout: ReturnType<typeof setTimeout>;
+
+for (const element of document.querySelectorAll<HTMLElement>(
+  "[data-tooltip]",
+)) {
+  element.addEventListener("pointerenter", () => {
+    clearTimeout(timeout);
+    const rect = element.getBoundingClientRect();
+    tooltip.textContent = element.getAttribute("data-tooltip");
+    tooltip.classList.add("tooltip--visible");
+    tooltip.style.left = `${rect.left + rect.width / 2}px`;
+    tooltip.style.top = `${rect.top}px`;
+  });
+
+  element.addEventListener("pointerleave", () => {
+    timeout = setTimeout(
+      () => tooltip.classList.remove("tooltip--visible"),
+      3000,
+    );
+  });
+}
+
+document.addEventListener("pointerdown", (event) => {
+  if (!(event.target as HTMLElement).closest("[data-tooltip]")) {
+    tooltip.classList.remove("tooltip--visible");
+  }
+});
